04958e8f68a2276ed4fe3cc34a76b79569badb57
Hubert Deng <hubertdeng123@gmail.com>
2024-08-29 15:22:03 -0700

    config validation and typing

--- src/commands/list_dependencies.py
+++ src/commands/list_dependencies.py
@@ -1,11 +1,10 @@
 from __future__ import annotations
 
 import os
-from argparse import _SubParsersAction
-from argparse import ArgumentParser
-from argparse import Namespace
+from argparse import ArgumentParser, Namespace, _SubParsersAction
 
 import yaml
+
 from utils.config import load_devservices_config
 from utils.devenv import get_code_root
 
@@ -28,24 +27,26 @@ def list_dependencies(args: Namespace) -> None:
     service_name = args.service_name
     if not service_name:
         current_dir = os.getcwd()
-        service_name = os.path.basename(current_dir)
-
-    code_root = get_code_root()
-    service_path = os.path.join(code_root, service_name)
+        service_path = os.path.join(current_dir)
+    else:
+        code_root = get_code_root()
+        service_path = os.path.join(code_root, service_name)
     try:
         config = load_devservices_config(service_path)
     except FileNotFoundError:
-        print(f'Service "{service_name}" not found')
+        print(
+            f'Service "{service_name}" not found, or devservices config does not exist'
+        )
         return
     except yaml.YAMLError as e:
         raise Exception(f"Failed to load service config: {e}")
 
-    dependencies = config.get("dependencies", {})
+    dependencies = config.devservices_config.dependencies
 
     if not dependencies:
         print(f"No dependencies found for {service_name}")
         return
 
-    print(f"Dependencies of {service_name}:")
+    print(f"Dependencies of {config.devservices_config.service_name}:")
     for dependency_key, dependency_info in dependencies.items():
-        print("-", dependency_key, ":", dependency_info["description"])
+        print("-", dependency_key, ":", dependency_info.description)
--- src/utils/config.py
+++ src/utils/config.py
@@ -1,11 +1,88 @@
 from __future__ import annotations
 
 import os
-from typing import Dict
+from typing import Dict, List, Optional, Union
 
 import yaml
-from constants import DEVSERVICES_DIR_NAME
-from constants import DOCKER_COMPOSE_FILE_NAME
+from pydantic import BaseModel, Field, model_validator, validator
+
+from constants import DEVSERVICES_DIR_NAME, DOCKER_COMPOSE_FILE_NAME
+
+
+class Dependency(BaseModel):
+    description: str
+
+
+class HealthCheck(BaseModel):
+    test: str
+
+
+class Ulimits(BaseModel):
+    nofile: Dict[str, int]
+
+
+class ServiceDefinition(BaseModel):
+    image: str
+    healthcheck: Optional[HealthCheck] = None
+    ulimits: Optional[Ulimits] = None
+    ports: Optional[List[str]] = None
+    environment: Optional[Dict[str, str]] = None
+    volumes: Optional[List[str]] = None
+
+
+class DevservicesConfig(BaseModel):
+    version: float
+    service_name: str
+    dependencies: Dict[str, Dependency]
+    modes: Dict[str, List[str]]
+
+    @validator("version")
+    def check_version(cls, version: float) -> float:
+        if version != 0.1:
+            raise ValueError("Version must be 0.1")
+        return version
+
+    @validator("modes")
+    def check_modes(
+        cls,
+        modes: Dict[str, List[str]],
+        values: Dict[str, Union[float, str, Dict[str, Dependency]]],
+    ) -> Dict[str, List[str]]:
+        dependencies: Dict[str, Dependency] = values.get("dependencies", {})
+        for mode, services in modes.items():
+            for service in services:
+                if service not in dependencies:
+                    raise ValueError(
+                        f"Service '{service}' in mode '{mode}' is not defined in dependencies"
+                    )
+        return modes
+
+
+class Config(BaseModel):
+    devservices_config: DevservicesConfig = Field(alias="x-sentry-devservices-config")
+    services: Dict[str, ServiceDefinition]
+    volumes: Optional[Dict[str, None]]
+
+    @model_validator(mode="after")
+    def check_services_match(self) -> Config:
+        dev_config = self.devservices_config
+        services = self.services
+
+        # Check if all dependencies are defined in services
+        for service in dev_config.dependencies:
+            if service not in services:
+                raise ValueError(
+                    f"Service '{service}' defined in x-sentry-devservices-config is not present in services"
+                )
+
+        # Check if all services are defined in dependencies
+        for service in services:
+            if service not in dev_config.dependencies:
+                raise ValueError(
+                    f"Service '{service}' defined in services is not present in x-sentry-devservices-config dependencies"
+                )
+
+        return self
 
 
 def load_devservices_config(service_path: str) -> Dict[str, Dict[str, str]]:
@@ -18,16 +95,7 @@ def load_devservices_config(service_path: str) -> Dict[str, Dict[str, str]]:
     with open(config_path, "r") as stream:
         try:
             config = yaml.safe_load(stream)
-            devservices_config = config.get("x-sentry-devservices-config")
-            if devservices_config is None:
-                raise KeyError(
-                    "Key 'x-sentry-devservices-config' not found in the config file."
-                )
-            if not isinstance(devservices_config, dict):
-                raise TypeError(
-                    "Value of 'x-sentry-devservices-config' must be a dictionary."
-                )
-            return devservices_config
+            return Config(**config)
         except FileNotFoundError as fnf:
             raise FileNotFoundError(f"Config file not found: {config_path}") from fnf
         except yaml.YAMLError as yml_error:
