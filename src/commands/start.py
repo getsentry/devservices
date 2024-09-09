cd434a8c1f62b1de7fbefa508e7868f652c991cf
Hubert Deng <hubertdeng123@gmail.com>
2024-09-09 16:36:19 -0700

    add start and stop

--- src/commands/start.py
+++ src/commands/start.py
@@ -1,27 +1,48 @@
 from __future__ import annotations
 
+import os
+import subprocess
 from argparse import _SubParsersAction
 from argparse import ArgumentParser
 from argparse import Namespace
 
+from constants import DEVSERVICES_DIR_NAME
+from constants import DOCKER_COMPOSE_FILE_NAME
 from utils.services import find_matching_service
 
 
 def add_parser(subparsers: _SubParsersAction[ArgumentParser]) -> None:
     parser = subparsers.add_parser("start", help="Start a service and its dependencies")
-    parser.add_argument("service_name", help="Name of the service to start")
+    parser.add_argument(
+        "service_name", help="Name of the service to start", nargs="?", default=None
+    )
     parser.set_defaults(func=start)
 
 
 def start(args: Namespace) -> None:
     """Start a service and its dependencies."""
     service_name = args.service_name
-
     try:
         service = find_matching_service(service_name)
     except Exception as e:
         print(e)
         exit(1)
-    # Implementation here
-    print(f"Starting service: {service.name}")
-    # Use docker_compose utility to start the service
+    modes = service.config.modes
+    # TODO: allow custom modes to be used
+    mode_to_start = "default"
+    mode_dependencies = modes[mode_to_start]
+    service_config_file_path = os.path.join(
+        service.repo_path, DEVSERVICES_DIR_NAME, DOCKER_COMPOSE_FILE_NAME
+    )
+    subprocess.run(
+        [
+            "docker",
+            "compose",
+            "-f",
+            service_config_file_path,
+            "up",
+            "-d",
+        ]
+        + mode_dependencies,
+        check=False,
+    )
