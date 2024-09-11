fa2d87663a66f51f84bb53ecee69693fcbb6cbe3
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-09-11 14:25:02 -0700

    Updating tests

--- tests/configs/test_service_config.py
+++ tests/configs/test_service_config.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import asdict
 from pathlib import Path
 
 import pytest
@@ -9,67 +10,63 @@ from exceptions import ConfigNotFoundError
 from exceptions import ConfigValidationError
 
 
+def create_config_file(tmp_path: Path, config: dict[str, dict[str, object]]) -> None:
+    devservices_dir = Path(tmp_path, "devservices")
+    devservices_dir.mkdir(parents=True, exist_ok=True)
+    tmp_file = Path(devservices_dir, "docker-compose.yml")
+    with tmp_file.open("w") as f:
+        yaml.dump(config, f, sort_keys=False, default_flow_style=False)
+
+
 @pytest.mark.parametrize(
     "service_name, dependencies, modes",
     [
         (
             "example-service",
-            {"example-dependency": "Example dependency"},
+            {"example-dependency": {"description": "Example dependency"}},
             {"default": ["example-dependency"]},
         ),
         (
             "example-service",
             {
-                "example-dependency-1": "Example dependency 1",
-                "example-dependency-2": "Example dependency 2",
+                "example-dependency-1": {
+                    "description": "Example dependency 1",
+                    "link": "https://example.com",
+                },
+                "example-dependency-2": {
+                    "description": "Example dependency 2",
+                },
             },
             {"default": ["example-dependency-1", "example-dependency-2"]},
-        ),
-        (
-            "example-service",
-            {"example-dependency": "Example dependency"},
-            {
-                "default": ["example-dependency"],
-                "debug": ["example-dependency"],
-                "test": ["example-dependency"],
-            },
         ),
     ],
 )
 def test_load_service_config_from_file(
     tmp_path: Path,
     service_name: str,
-    dependencies: dict[str, str],
+    dependencies: dict[str, dict[str, object]],
     modes: dict[str, list[str]],
 ) -> None:
-    devservices_dir = tmp_path / "devservices"
-    devservices_dir.mkdir(parents=True, exist_ok=True)
-    tmp_file = devservices_dir / "docker-compose.yml"
     config = {
         "x-sentry-service-config": {
             "version": 0.1,
             "service_name": service_name,
-            "dependencies": {
-                key: {"description": value} for key, value in dependencies.items()
-            },
-            "modes": modes,
+            "dependencies": {key: value for key, value in dependencies.items()},
+            "modes": {key: value for key, value in modes.items()},
         }
     }
-    with tmp_file.open("w") as f:
-        yaml.dump(config, f, sort_keys=False, default_flow_style=False)
-    # We pass the tmp_path rather than the tmp_file because the function expects the directory
+    create_config_file(tmp_path, config)
+
     service_config = load_service_config_from_file(tmp_path)
-    assert service_config.version == 0.1
-    assert service_config.service_name == service_name
-    assert len(service_config.dependencies) == len(dependencies)
-    for key, value in dependencies.items():
-        assert key in service_config.dependencies
-        assert service_config.dependencies[key].description == value
-    assert len(service_config.modes) == len(modes)
-    assert "default" in service_config.modes
-    for mode, services in modes.items():
-        assert mode in service_config.modes
-        assert service_config.modes[mode] == services
+    assert asdict(service_config) == {
+        "version": 0.1,
+        "service_name": service_name,
+        "dependencies": {
+            key: {"description": value["description"], "link": value.get("link")}
+            for key, value in dependencies.items()
+        },
+        "modes": modes,
+    }
 
 
 def test_load_service_config_from_file_missing_config(tmp_path: Path) -> None:
@@ -82,9 +79,6 @@ def test_load_service_config_from_file_missing_config(tmp_path: Path) -> None:
 
 
 def test_load_service_config_from_file_invalid_version(tmp_path: Path) -> None:
-    devservices_dir = tmp_path / "devservices"
-    devservices_dir.mkdir(parents=True, exist_ok=True)
-    tmp_file = devservices_dir / "docker-compose.yml"
     config = {
         "x-sentry-service-config": {
             "version": 0.2,
@@ -95,17 +89,14 @@ def test_load_service_config_from_file_invalid_version(tmp_path: Path) -> None:
             "modes": {"default": ["example-dependency"]},
         }
     }
-    with tmp_file.open("w") as f:
-        yaml.dump(config, f, sort_keys=False, default_flow_style=False)
+    create_config_file(tmp_path, config)
+
     with pytest.raises(ConfigValidationError) as e:
         load_service_config_from_file(tmp_path)
     assert str(e.value) == "Invalid version '0.2' in service config"
 
 
 def test_load_service_config_from_file_missing_service_name(tmp_path: Path) -> None:
-    devservices_dir = tmp_path / "devservices"
-    devservices_dir.mkdir(parents=True, exist_ok=True)
-    tmp_file = devservices_dir / "docker-compose.yml"
     config = {
         "x-sentry-service-config": {
             "version": 0.1,
@@ -115,8 +106,7 @@ def test_load_service_config_from_file_missing_service_name(tmp_path: Path) -> N
             "modes": {"default": ["example-dependency"]},
         }
     }
-    with tmp_file.open("w") as f:
-        yaml.dump(config, f, sort_keys=False, default_flow_style=False)
+    create_config_file(tmp_path, config)
 
     with pytest.raises(ConfigValidationError) as e:
         load_service_config_from_file(tmp_path)
@@ -124,9 +114,6 @@ def test_load_service_config_from_file_missing_service_name(tmp_path: Path) -> N
 
 
 def test_load_service_config_from_file_invalid_dependency(tmp_path: Path) -> None:
-    devservices_dir = tmp_path / "devservices"
-    devservices_dir.mkdir(parents=True, exist_ok=True)
-    tmp_file = devservices_dir / "docker-compose.yml"
     config = {
         "x-sentry-service-config": {
             "version": 0.1,
@@ -137,8 +124,8 @@ def test_load_service_config_from_file_invalid_dependency(tmp_path: Path) -> Non
             "modes": {"default": ["example-dependency", "unknown-dependency"]},
         }
     }
-    with tmp_file.open("w") as f:
-        yaml.dump(config, f, sort_keys=False, default_flow_style=False)
+    create_config_file(tmp_path, config)
+
     with pytest.raises(ConfigValidationError) as e:
         load_service_config_from_file(tmp_path)
     assert (
