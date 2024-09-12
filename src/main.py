1074c479062ac3a37935c79f45341e9819113a73
Hubert Deng <hubertdeng123@gmail.com>
2024-09-12 16:35:59 -0700

    add sentry

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 import argparse
 
+import sentry_sdk
 from commands import list_dependencies
 from commands import list_services
 from commands import logs
 from commands import start
 from commands import status
 from commands import stop
+from sentry_sdk.integrations.argv import ArgvIntegration
 
 
 def main() -> None:
@@ -28,9 +30,22 @@ def main() -> None:
 
     args = parser.parse_args()
 
+    # Set up Sentry
+    sentry_sdk.init(
+        dsn="https://e9f6505edbde9a6b4528b7f56ce0c508@o1383316.ingest.us.sentry.io/4507942331875328",
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        traces_sample_rate=1.0,
+        # Enable performance monitoring
+        enable_tracing=True,
+        # Enable the Argv integration
+        integrations=[ArgvIntegration()],
+    )
+
     if args.command:
         # Call the appropriate function based on the command
-        args.func(args)
+        with sentry_sdk.start_transaction(op="command", name=args.command):
+            args.func(args)
     else:
         parser.print_help()
 
