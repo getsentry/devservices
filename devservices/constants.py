b0e4ff15225c6032b202144b3f2bfc558764bf82
Hubert Deng <hubertdeng123@gmail.com>
2024-09-30 15:12:29 -0700

    add docker compose version check

--- devservices/constants.py
+++ devservices/constants.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import os
 
+MINIMUM_DOCKER_COMPOSE_VERSION = "2.21.0"
 DEVSERVICES_DIR_NAME = "devservices"
 CONFIG_FILE_NAME = "config.yml"
 
--- devservices/main.py
+++ devservices/main.py
@@ -3,9 +3,13 @@ from __future__ import annotations
 import argparse
 import atexit
 import os
+import re
+import subprocess
 from importlib import metadata
+from typing import cast
 
 import sentry_sdk
+from packaging import version
 from sentry_sdk.integrations.argv import ArgvIntegration
 
 from devservices.commands import list_dependencies
@@ -14,6 +18,8 @@ from devservices.commands import logs
 from devservices.commands import start
 from devservices.commands import status
 from devservices.commands import stop
+from devservices.constants import MINIMUM_DOCKER_COMPOSE_VERSION
+from devservices.exceptions import DockerComposeError
 
 sentry_environment = (
     "development" if os.environ.get("IS_DEV", default=False) else "production"
@@ -29,12 +35,55 @@ sentry_sdk.init(
 )
 
 
+def check_docker_compose_version() -> None:
+    cmd = ["docker", "compose", "version", "--short"]
+    try:
+        # Run the docker compose version command
+        result = subprocess.run(
+            cmd,
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        # Extract the version number from the output
+        version_output = result.stdout.strip()
+
+        # Use regex to find the version number
+        pattern = r"^(\d+\.\d+\.\d+)"
+
+        match = re.search(pattern, version_output)
+        if match:
+            # There is a chance that Any type is returned, so cast this
+            docker_compose_version = cast(str, match.group(1))
+        else:
+            docker_compose_version = None
+
+    except subprocess.CalledProcessError as e:
+        raise DockerComposeError(
+            command=" ".join(cmd),
+            returncode=e.returncode,
+            stdout=e.stdout,
+            stderr=e.stderr,
+        )
+
+    if docker_compose_version is None:
+        print("Unable to detect docker compose version")
+        exit(1)
+    elif version.parse(docker_compose_version) < version.parse(
+        MINIMUM_DOCKER_COMPOSE_VERSION
+    ):
+        print("Docker compose version unsupported, please upgrade to >= 2.21.0")
+        exit(1)
+
+
 @atexit.register
 def cleanup() -> None:
     sentry_sdk.flush()
 
 
 def main() -> None:
+    check_docker_compose_version()
     parser = argparse.ArgumentParser(
         prog="devservices",
         description="CLI tool for managing service dependencies.",
