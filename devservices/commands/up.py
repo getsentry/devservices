41295f973b524a6004b48a4a0b4e9335892ebe9a
Hubert Deng <hubertdeng123@gmail.com>
2024-11-20 15:22:50 -0800

    add testing

--- devservices/commands/up.py
+++ devservices/commands/up.py
@@ -142,5 +142,6 @@ def _create_devservices_network() -> None:
     subprocess.run(
         ["docker", "network", "create", "devservices"],
         capture_output=True,
-        check=True,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
     )
