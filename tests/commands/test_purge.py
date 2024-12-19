a2a63e354b4da17f710e7674e10ebadbd47269ff
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-12-19 10:55:33 -0600

    Adding more test coverage

--- tests/commands/test_purge.py
+++ tests/commands/test_purge.py
@@ -69,7 +69,7 @@ def test_purge_docker_daemon_not_running(
 @mock.patch("devservices.commands.purge.get_matching_containers")
 @mock.patch("devservices.commands.purge.stop_containers")
 @mock.patch("devservices.commands.purge.subprocess.run")
-def test_purge_docker_daemon_docker_error(
+def test_purge_docker_error_find_matching_containers(
     mock_run: mock.Mock,
     mock_stop_containers: mock.Mock,
     mock_get_matching_containers: mock.Mock,
@@ -117,6 +117,63 @@ def test_purge_docker_daemon_docker_error(
 
         captured = capsys.readouterr()
         assert "Failed to get devservices containers stderr" in captured.out.strip()
+
+
+@mock.patch("devservices.commands.purge.get_matching_containers")
+@mock.patch("devservices.commands.purge.stop_containers")
+@mock.patch("devservices.commands.purge.subprocess.run")
+def test_purge_docker_error_stop_containers(
+    mock_run: mock.Mock,
+    mock_stop_containers: mock.Mock,
+    mock_get_matching_containers: mock.Mock,
+    capsys: pytest.CaptureFixture[str],
+    tmp_path: Path,
+) -> None:
+    mock_get_matching_containers.return_value = ["abc", "def", "ghi"]
+    mock_stop_containers.side_effect = DockerError("command", 1, "output", "stderr")
+    with (
+        mock.patch(
+            "devservices.commands.purge.DEVSERVICES_CACHE_DIR",
+            str(tmp_path / ".devservices-cache"),
+        ),
+        mock.patch("devservices.utils.state.STATE_DB_FILE", str(tmp_path / "state")),
+        mock.patch(
+            "devservices.commands.purge.subprocess.check_output",
+            return_value=b"",
+        ),
+    ):
+        # Create a cache file to test purging
+        cache_dir = tmp_path / ".devservices-cache"
+        cache_dir.mkdir(parents=True, exist_ok=True)
+        cache_file = tmp_path / ".devservices-cache" / "test.txt"
+        cache_file.write_text("This is a test cache file.")
+
+        state = State()
+        state.update_started_service("test-service", "test-mode")
+
+        assert cache_file.exists()
+        assert state.get_started_services() == ["test-service"]
+
+        args = Namespace()
+        with pytest.raises(SystemExit):
+            purge(args)
+
+        assert not cache_file.exists()
+        assert state.get_started_services() == []
+
+        mock_get_matching_containers.assert_called_once_with(
+            DEVSERVICES_ORCHESTRATOR_LABEL
+        )
+        mock_stop_containers.assert_called_once_with(
+            ["abc", "def", "ghi"], should_remove=True
+        )
+        mock_run.assert_not_called()
+
+        captured = capsys.readouterr()
+        assert (
+            "Failed to stop running devservices containers stderr"
+            in captured.out.strip()
+        )
 
 
 @mock.patch("devservices.commands.purge.stop_containers")
