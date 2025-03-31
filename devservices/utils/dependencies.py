830d5c72892ff46991253903bf08cd40daf90023
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2025-03-31 09:25:54 -0700

    Removing redundant logic

--- devservices/utils/dependencies.py
+++ devservices/utils/dependencies.py
@@ -80,9 +80,6 @@ class DependencyGraph:
             self.graph[node] = set()
 
     def add_edge(self, from_node: DependencyNode, to_node: DependencyNode) -> None:
-        if from_node == to_node:
-            if from_node.dependency_type == to_node.dependency_type:
-                return
         if from_node not in self.graph:
             self.add_node(from_node)
         if to_node not in self.graph:
