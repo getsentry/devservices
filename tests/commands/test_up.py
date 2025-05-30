9ca79248c87e34a4d3e917602493494793ac3d78
Hubert Deng <hubertdeng123@gmail.com>
2025-05-30 15:02:07 -0700

    fix tests

--- tests/commands/test_up.py
+++ tests/commands/test_up.py
@@ -2654,7 +2654,7 @@ def test_bring_up_supervisor_programs_no_programs_config(
 
     with pytest.raises(
         SupervisorConfigError,
-        match=f"No programs.conf file found in {tmp_path / DEVSERVICES_DIR_NAME / PROGRAMS_CONF_FILE_NAME}",
+        match=f"Config file {tmp_path / DEVSERVICES_DIR_NAME / PROGRAMS_CONF_FILE_NAME} does not exist",
     ):
         os.chdir(tmp_path)
         bring_up_supervisor_programs(service, ["supervisor-program"], status)
